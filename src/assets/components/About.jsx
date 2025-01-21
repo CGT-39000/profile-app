ca41ebc69c20042d88125a1340d715290f0d2083
xM4ddy <59680197+xM4ddy@users.noreply.github.com>
2025-01-21 16:17:51 -0500

    add components

---
+++ src/assets/components/About.jsx
@@ -0,0 +1,12 @@
+const About = () => {
+	return(
+		<div>
+			<h2>Profile</h2>
+			<p>
+				This is a single page profile app.
+			</p>
+		</div>
+	)
+}
+
+export default About;
---
+++ src/assets/components/Navbar.jsx
@@ -0,0 +1,19 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
