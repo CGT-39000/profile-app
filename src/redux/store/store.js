a871801b976f175b65c01d85f59c3a9617532662
xM4ddy <59680197+xM4ddy@users.noreply.github.com>
2025-04-02 22:14:01 -0400

    redux 2

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modeSlice from "../slices/modeSlice";
+import authReducer from "../slices/authSlice";
 
 const store = configureStore({
 	reducer: {
 		mode: modeSlice,
+		auth: authReducer,
 	}
 })
 
